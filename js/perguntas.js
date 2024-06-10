040aac5222c864fcec6f3155c2b96ad578631417
Ray Grillo Fujiyoshi <111706736+RayGrillo@users.noreply.github.com>
2024-06-10 09:58:25 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,173 @@
+export const perguntas = [
+    {
+        enunciado: "BBB",
+        alternativas: [
+            {
+                texto: "CCC",
+                afirmacao: [
+                    "CCC",
+                    "ccc"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "DDD",
+                afirmacao: [
+                    "DDD",
+                    "ddd"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "EEE",
+        alternativas: [
+            {
+                texto: "FFF",
+                afirmacao: [
+                    "FFF",
+                    "fff",
+                    "FfF"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "GGG",
+                afirmacao: [
+                    "GGG",
+                    "ggg",
+                    "GgG"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "HHH",
+        alternativas: [
+            {
+                texto: "III",
+                afirmacao: [
+                    "III",
+                    "iii",
+                    "IiI"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "JJJ",
+                afirmacao: [
+                    "JJJ",
+                    "jjj",
+                    "JjJ"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "KKK",
+        alternativas: [
+            {
+                texto: "LLL",
+                afirmacao: [
+                    "LLL",
+                    "lll"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "MMM",
+                afirmacao: [
+                    "MMM",
+                    "mmm"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "NNN",
+        alternativas: [
+            {
+                texto: "OOO",
+                afirmacao: [
+                    "OOO",
+                    "ooo"
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "PPP",
+                afirmacao: [
+                    "PPP",
+                    "ppp"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "QQQ",
+        alternativas: [
+            {
+                texto: "RRR",
+                afirmacao: [
+                    "RRR",
+                    "rrr"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "SSS",
+                afirmacao: [
+                    "SSS",
+                    "sss"
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "TTT",
+        alternativas: [
+            {
+                texto: "UUU",
+                afirmacao: [
+                    "UUU",
+                    "uuu"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "VVV",
+                afirmacao: [
+                    "VVV",
+                    "vvv"
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "WWW",
+        alternativas: [
+            {
+                texto: "YYY",
+                afirmacao: [
+                    "YYY",
+                    "yyy"
+                ],
+            },
+            {
+                texto: "ZZZ",
+                afirmacao: [
+                    "ZZZ",
+                    "zzz",
+                    "ZzZ"
+                ],
+            },
+        ]
+    }
+];
